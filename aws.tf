dd31a30cc3aa014e6a8fe2fce8d1516163b2b18d
ins0mia <tanya.kravchenko.v@gmail.com>
2017-04-10 16:34:34 +0300

    del s3 entity

--- aws.tf
+++ aws.tf
@@ -10,24 +10,3 @@ terraform {
 provider "aws" {
   region = "${var.aws_region}"
 }
-
-resource "aws_s3_bucket" "state" {
-  bucket = "${var.project_name}-terraform-state"
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    enabled = true
-    prefix = "/"
-    noncurrent_version_expiration {
-      days = 30
-    }
-  }
-
-  tags {
-    Name = "${var.project_name} terraform bucket"
-  }
-}
